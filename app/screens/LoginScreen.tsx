5770adbfb7aa9daea053a6578d436af7cab32c9b
Laurent Binder <laurentbinder@gmail.com>
2024-05-08 22:20:38 +0200

    add Formik and custom errors

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -8,11 +8,13 @@ import {
   KeyboardAvoidingView,
   StyleSheet,
   TextInput,
+  Text,
   View,
 } from 'react-native';
 import { FIREBASE_AUTH } from '../../FirebaseConfig';
 import { Button } from '../components/Button';
 import * as yup from 'yup';
+import { Formik, Field, ErrorMessage } from 'formik';
 
 const validationSchema = yup.object().shape({
   email: yup.string().email('Invalid email').required('Email is required'),
@@ -23,19 +25,23 @@ const validationSchema = yup.object().shape({
 });
 
 export const LoginScreen = ({}) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const auth = FIREBASE_AUTH;
 
-  const signIn = async () => {
+  const signIn = async (values) => {
     setLoading(true);
     try {
       await validationSchema.validate(
         { email, password },
         { abortEarly: false }
       );
-      const response = await signInWithEmailAndPassword(auth, email, password);
+      const response = await signInWithEmailAndPassword(
+        auth,
+        values.email,
+        values.password
+      );
       console.log('🚀 ~ signIn ~ response:', response);
     } catch (error) {
       if (error instanceof yup.ValidationError) {
@@ -49,18 +55,16 @@ export const LoginScreen = ({}) => {
     }
   };
 
-  const signUp = async () => {
+  const signUp = async (values) => {
+    console.log('🚀 ~ signUp ~ values:', values);
     setLoading(true);
     try {
-      await validationSchema.validate(
-        { email, password },
-        { abortEarly: false }
-      );
+      await validationSchema.validate(values, { abortEarly: false });
 
       const response = await createUserWithEmailAndPassword(
         auth,
-        email,
-        password
+        values.email,
+        values.password
       );
       console.log('🚀 ~ signIn ~ response:', response);
       alert('Check your emails!');
@@ -77,36 +81,68 @@ export const LoginScreen = ({}) => {
   };
 
   return (
-    <View style={styles.mainContainer}>
-      <KeyboardAvoidingView style={styles.container} behavior='padding'>
-        <View>
-          <TextInput
-            value={email}
-            style={styles.input}
-            placeholder='Email'
-            autoCapitalize='none'
-            onChangeText={(text) => setEmail(text)}
-          ></TextInput>
-          <TextInput
-            secureTextEntry={true}
-            value={password}
-            style={styles.input}
-            placeholder='Password'
-            autoCapitalize='none'
-            onChangeText={(text) => setPassword(text)}
-          ></TextInput>
-        </View>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log('🚀 ~ values:', values);
+        signIn(values);
+        setSubmitting(false);
+      }}
+    >
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        errors,
+        touched,
+        isSubmitting,
+      }) => (
+        <View style={styles.mainContainer}>
+          <KeyboardAvoidingView style={styles.container} behavior='padding'>
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder='Email'
+                autoCapitalize='none'
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+              />
+              <Text style={styles.error}>
+                {touched.email && errors.email ? errors.email : ''}
+              </Text>
+              <TextInput
+                style={styles.input}
+                secureTextEntry={true}
+                placeholder='Password'
+                autoCapitalize='none'
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+              />
+              <Text style={styles.error}>
+                {touched.password && errors.password ? errors.password : ''}
+              </Text>
+            </View>
 
-        {loading ? (
-          <ActivityIndicator size='large' color='#0000ff' />
-        ) : (
-          <View style={styles.btnContainer}>
-            <Button title='Login' onPress={signIn} />
-            <Button title='Create account' onPress={signUp} />
-          </View>
-        )}
-      </KeyboardAvoidingView>
-    </View>
+            {loading ? (
+              <ActivityIndicator size='large' color='#0000ff' />
+            ) : (
+              <View style={styles.btnContainer}>
+                <Button
+                  title='Login'
+                  onPress={handleSubmit}
+                  disabled={isSubmitting}
+                />
+                <Button title='Create account' onPress={() => signUp(values)} />
+              </View>
+            )}
+          </KeyboardAvoidingView>
+        </View>
+      )}
+    </Formik>
   );
 };
 
@@ -125,5 +161,9 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     backgroundColor: '#fff',
     paddingHorizontal: 20,
+  },
+  error: {
+    color: 'red',
+    // paddingBottom: 10,
   },
 });
